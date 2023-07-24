0a561d4cd19e53980a0ef2557aa1d0485fff5c52
waohu0902 <139087473+waohu0902@users.noreply.github.com>
2023-07-24 09:34:59 +0800

    Add files via upload

---
+++ client_3.c
@@ -0,0 +1,370 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <openssl/bio.h>
+#include <openssl/ssl.h>
+#include <openssl/des.h>
+#include "openssl/evp.h"
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <sys/utsname.h>
+#include <openssl/md5.h>
+#pragma comment(lib,"libssl.lib")
+#pragma comment(lib,"libcrypto.lib")
+#include <iostream>
+#pragma warning(disable:4996)
+using namespace std;
+
+// 宏定义
+#define     MD5_LENGTH           16
+#define     MAX                  1024
+
+ // 定义数据
+int c_fd; // 客户端套接字定义
+int n_write; // 写标志位
+char readbuf[MAX]={'\0'};
+char writebuf[MAX]={'\0'}; // 存放待传数据的字符数组
+char mode[2]={'\0'}; // 传输模式标志
+char half_opt[2]={'\0'}; // 文件传输内标志位
+char cry_flag[2]={'\0'}; // 加密标志位
+struct sockaddr_in s_addr; // 服务端地址
+char end_flag[4]={'/','E','n','d'}; // 传输结束标志位
+char exit_flag[5]={'/','E','x','i','t'}; // 退出数据传输模式标志位
+int i=0; // 计数位1
+int count=0; // 计数位2
+FILE *fp; // 文件位置
+char filename[100]={'\0'}; // 文件名
+DES_cblock key="1234567"; // 密钥
+DES_key_schedule key_schedule; // 加密用schedule
+unsigned  char input[MAX]={'\0'}; // 加密输入/解密输出
+size_t len = (sizeof(input) + 7) / 8 * 8; // 定义输出长度
+unsigned char* output = new unsigned char[len + 1]; // 加密输出/解密输入
+DES_cblock ivec; // IV
+char judge[5]={'\0'}; // 程序判别位
+int judge_flag=0; // 判别结果储存
+char check_flag[128]={'\0'};
+char check[2]={'\0'};
+char data_store[MAX]={'\0'};
+
+// 功能函数
+
+// 加密函数
+void encry(void)
+{
+	DES_set_key(&key, &key_schedule); // 根据key产生schedule
+	memset((char*)&ivec, 0, sizeof(ivec)); //设置IV为0x0000000000000000
+	memset(input,0,MAX); // 清空数组
+	memset(output,0,MAX);
+	memcpy(input,writebuf,MAX);
+	DES_ncbc_encrypt(input, output, sizeof(input), &key_schedule, &ivec, DES_ENCRYPT); //加密
+	printf("加密结果：\n");
+	memset(writebuf,0,MAX);
+	memcpy(writebuf,output,MAX);
+	printf("%s\n",writebuf); // 打印加密输出
+	memset((char*)&ivec, 0, sizeof(ivec)); //设置IV为0x0000000000000000
+}
+
+// 模式判别
+int judge_prog(void)
+{
+	count = 0;
+	for(i=0;i<4;i++)
+        {
+        	if(end_flag[i]==judge[i])
+        	{
+        		count++;
+        	}
+        	else
+        	break;
+        }
+        if(count==4)
+        {
+        	return -1;
+        }
+	count = 0;
+	for(i=0;i<5;i++)
+        {
+        	if(exit_flag[i]==judge[i])
+        	{
+        		count++;
+        	}
+        	else
+        	break;
+        }
+        if(count==5)
+        {
+        	return 1;
+        }
+	return 0;
+}
+
+// 数据传递函数
+void datatran(void)
+{
+        puts("请输入待传数据（输入‘/End’将退出程序）："); // 人机交互提示
+	memset(writebuf,0,MAX); // 清空数组
+	memset(judge,0,sizeof(judge)); // 清空数组
+	getchar();
+        scanf("%[^\n]",writebuf); // 采集待传数据
+	memset(data_store,0,sizeof(data_store));
+	strcpy(data_store,writebuf);
+	for(i=0;i<5;i++)
+	{
+		judge[i]=writebuf[i];
+	} // 将采集到的前5位字符存入judge数组
+	judge_flag=judge_prog();
+	if((judge_flag==-1)||(judge_flag==1));
+	else
+	{
+		if(cry_flag[0]=='Y')
+		{
+			encry();
+		} // 用户选定加密传输时对数据进行加密
+	}
+	n_write=write(c_fd,writebuf,MAX); // 向server端发送数据
+}
+  
+// 文件传递函数 
+void filetran(void)
+{
+	int ch;
+	char writestr[2]={'\0'};
+ 	puts("请输入待传文件路径："); //人机交互，提示输入文件路径
+        scanf("%s",filename); // 采集文件路径
+        if((fp=fopen(filename,"r"))==NULL)
+	{
+		while((fp=fopen(filename,"r"))==NULL)
+		{
+			puts("打开文件时出错"); // 找不到路径时提示
+		 	puts("请输入待传文件路径："); //人机交互，提示输入文件路径
+        		scanf("%s",filename); // 采集文件路径
+		}
+	}
+	while ((ch = fgetc(fp)) != EOF)
+	{
+		writestr[0]=ch;
+		strcat(writebuf,writestr);
+	}
+	fclose(fp);
+	printf("%s",writebuf);
+	strcpy(data_store,writebuf);
+	if(cry_flag[0]=='Y')
+	{
+		encry();
+	}
+	n_write=write(c_fd,writebuf,sizeof(writebuf)); // 发送文件
+}
+
+void hardware_tran_opera(void)
+{
+	struct utsname uts;
+	if (uname(&uts) == -1) 
+	{
+        	perror("uname error");
+        	exit(-1);
+    	}
+	memset(writebuf,0,sizeof(writebuf));
+	sprintf(writebuf,"Operating system: %s %s %s\n", uts.sysname, uts.release, uts.version);
+   	printf("Operating system: %s %s %s\n", uts.sysname, uts.release, uts.version);
+	n_write=write(c_fd,writebuf,sizeof(writebuf)); // 向server端发送数据
+}
+   
+void hardware_tran_CPUID(void)
+{
+	unsigned int s1,s2;
+	asm volatile
+    	( "movl $0x01,%%eax ; \n\t"
+      	"xorl %%edx,%%edx ;\n\t"
+      	"cpuid ;\n\t"
+      	"movl %%edx , %0;\n\t"
+      	"movl %%eax , %1;\n\t"
+      	:"=m"(s1),"=m"(s2)
+    	);
+    	char cpu_id[32] = {0};
+    	sprintf(cpu_id, "CPUID:%08X-%08X\n", htonl(s2), htonl(s1));
+	printf("%s",cpu_id);
+	memset(writebuf,0,sizeof(writebuf));
+	strcpy(writebuf,cpu_id);
+	n_write=write(c_fd,writebuf,sizeof(writebuf)); // 向server端发送数据
+}
+
+void hardware_tran_mac(void)
+{
+        int sock, if_count, i;
+        struct ifconf ifc;
+        struct ifreq ifr[10];
+        unsigned char mac[6];
+
+        memset(&ifc, 0, sizeof(struct ifconf));
+
+        sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+        ifc.ifc_len = 10 * sizeof(struct ifreq);
+        ifc.ifc_buf = (char *)ifr;
+        //获取所有网卡信息
+        ioctl(sock, SIOCGIFCONF, (char *)&ifc);
+
+        if_count = ifc.ifc_len / (sizeof(struct ifreq));
+        for (i = 0; i < if_count; i++) 
+	{        
+                if (ioctl(sock, SIOCGIFHWADDR, &ifr[i]) == 0) 
+		{  
+                        memcpy(mac, ifr[i].ifr_hwaddr.sa_data, 6);
+			memset(writebuf,0,sizeof(writebuf));
+                        sprintf(writebuf,"eth: %s, mac: %02x:%02x:%02x:%02x:%02x:%02x\n", ifr[i].ifr_name, mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+			printf("%s",writebuf);
+			n_write=write(c_fd,writebuf,sizeof(writebuf)); // 向server端发送数据
+                } 
+        }
+}
+
+void md5()
+{
+	MD5_CTX  ctx;
+	char  data[MAX];
+	unsigned char  md[MD5_LENGTH];
+	char buf[MAX] = "";
+	char tmp[3] = "";
+	int q=0;
+	strcpy(data,data_store);
+	data[strlen(data)-1] = '\0';
+	MD5_Init(&ctx);
+	MD5_Update(&ctx, data, strlen(data));
+	MD5_Final(md, &ctx);
+	for (q=0; q < MD5_LENGTH; q++)
+	{
+        sprintf(tmp, "%02X", md[q]);
+        strcat(buf, tmp);
+    	}
+	printf("完整性验证序列: ");
+	memset(check_flag,0,sizeof(check_flag));
+	strcpy(check_flag,buf);
+	printf("%s\n",check_flag);
+}
+
+int main(int argc,char** argv)
+{
+ 
+        if(argc!=3)
+	{
+                printf("调用格式不正确！\n"); // 调用格式出错时提示并退出
+                exit(-1);
+        }
+        c_fd=socket(AF_INET ,SOCK_STREAM,0); // 客户端套接字产生
+        if(c_fd==-1)
+	{
+                perror("socket"); // 套接字产生失败时报错
+        }
+        s_addr.sin_family=AF_INET; // IPV4
+        s_addr.sin_port=htons(atoi(argv[2])); // 端口
+        s_addr.sin_addr.s_addr=inet_addr(argv[1]); // 服务端地址
+        if(connect(c_fd,(struct sockaddr *)&s_addr,sizeof(struct sockaddr_in))==-1)
+	{
+                perror("connect"); // 连接失败时报错
+        }
+	while(1)
+	{
+		memset(readbuf,0,sizeof(readbuf));
+		read(c_fd,readbuf,sizeof(readbuf)); // 读操作，读取输入帐号提示
+		puts(readbuf);
+		memset(writebuf,0,sizeof(writebuf)); // 清空数组
+		scanf("%s",writebuf);
+		n_write=write(c_fd,writebuf,sizeof(writebuf)); // 写帐号
+		memset(readbuf,0,sizeof(readbuf));
+		read(c_fd,readbuf,sizeof(readbuf)); // 读操作，读取密码
+		puts(readbuf);
+		memset(readbuf,0,sizeof(readbuf));
+		memset(writebuf,0,sizeof(writebuf)); // 清空数组
+		scanf("%s",writebuf);
+		n_write=write(c_fd,writebuf,sizeof(writebuf)); // 写密码
+		read(c_fd,readbuf,sizeof(readbuf)); // 读操作，读取反馈
+		puts(readbuf);
+		if(strcmp(readbuf,"登录成功！")==0)
+		break;
+	}
+	hardware_tran_opera();
+	hardware_tran_CPUID();
+	hardware_tran_mac();
+	memset(readbuf,0,sizeof(readbuf));
+	read(c_fd,readbuf,sizeof(readbuf)); // 读操作
+	puts(readbuf);
+	if(strcmp(readbuf,"您的硬件设备无接入权限！")==0)
+	exit(-1);
+ 	while(1)
+	{
+ 		puts("请选择传输模式（D：传数据；F：传文件；E：结束传输）：");
+ 		scanf("%s",mode); // 采集用户模式设置指令
+		memset(writebuf,0,sizeof(writebuf)); // 清空数组
+		writebuf[0]=mode[0];
+		n_write=write(c_fd,writebuf,sizeof(writebuf)); // 告知服务端当前传递模式
+		if(mode[0]!='E')
+		{
+			puts("请选择是否加密（Y：是；N：否）：");
+			scanf("%s",cry_flag); // 采集用户加密指令
+			memset(writebuf,0,sizeof(writebuf)); // 清空数组
+			writebuf[0]=cry_flag[0];
+			n_write=write(c_fd,writebuf,sizeof(writebuf)); // 告知服务端加密与否
+		} // 非E指令下，用户选择是否加密传输，并传递给服务端
+ 		if(mode[0]=='D')
+ 		{
+ 			puts("当前工作在数据传输模式，输入“/Exit”以退出");
+ 			while(1)
+			{
+				memset(writebuf,0,sizeof(writebuf)); // 清空数组
+        			datatran();
+				if(judge_flag==-1)
+				exit(-1); //结束程序运行
+				if(judge_flag==1)
+				break; // 退出当前模式
+				memset(readbuf,0,sizeof(readbuf)); // 清空数组
+				read(c_fd,readbuf,sizeof(readbuf)); // 读操作
+				if(strcmp(readbuf,"check")==0)
+				{
+					md5();
+					memset(writebuf,0,sizeof(writebuf)); // 清空数组
+					strcpy(writebuf,check_flag);
+					n_write=write(c_fd,writebuf,sizeof(writebuf)); // 告知服务端
+				}
+        		}
+        	} // 程序工作在数据传输模式
+        	else if(mode[0]=='F')
+        	{
+        		puts("当前工作在文件传输模式");
+        		while(1)
+			{
+				memset(writebuf,0,sizeof(writebuf)); // 清空数组
+       				filetran();
+				memset(readbuf,0,sizeof(readbuf)); // 清空数组
+				read(c_fd,readbuf,sizeof(readbuf)); // 读操作
+				if(strcmp(readbuf,"check")==0)
+				{
+					md5();
+					memset(writebuf,0,sizeof(writebuf)); // 清空数组
+					strcpy(writebuf,check_flag);
+					n_write=write(c_fd,writebuf,sizeof(writebuf)); // 告知服务端
+				}
+       				puts("请选择（C：继续传输文件；E：退出当前模式）：");
+       				scanf("%s",half_opt);
+       				if(half_opt[0]=='C'); // 继续传输文件
+       				else
+       				break; // 退出文件传输模式
+        		}
+        	} // 程序工作在文件传输模式
+        	else
+        	{
+        		exit(-1); // 终止客户端程序
+        	} // 其他情况，包括E模式与错误指令输入
+        	if(n_write==-1)
+		{
+		        puts("write failed"); // 发送失败报错
+		        exit(-1);
+        	}
+        	memset(writebuf,0,sizeof(writebuf)); // 清空数组
+	}
+        return 0;
+}
---
+++ server_3.c
@@ -0,0 +1,465 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <openssl/bio.h>
+#include <openssl/ssl.h>
+#include <openssl/des.h>
+#include "openssl/evp.h"
+#include <openssl/md5.h>
+#pragma comment(lib,"libssl.lib")
+#pragma comment(lib,"libcrypto.lib")
+#include <iostream>
+#pragma warning(disable:4996)
+using namespace std;
+
+// 宏定义
+#define     MD5_LENGTH           16
+#define     MAX                  1024
+
+ // 定义数据
+int s_fd; // 服务端套接字定义
+int c_fd; // 客户端套接字定义
+char readbuf[MAX]={'\0'}; // 存放接收数据的字符数组
+char writebuf[MAX]={'\0'}; // 存放待传数据的字符数组
+struct sockaddr_in s_addr; // 服务端地址
+struct sockaddr_in c_addr; // 客户端地址
+char mode[2]={'\0'}; // 传输模式标志
+char decry_flag[2]={'\0'}; // 解密标志位
+char end_flag[4]={'/','E','n','d'}; // 传输结束标志位
+char exit_flag[5]={'/','E','x','i','t'}; // 退出数据传输模式标志位
+int i=0; // 计数位1
+int count=0; // 计数位2
+FILE *fp; // 文件位置
+char filename[100]={'\0'}; // 文件名
+char filename_write[100]={'\0'};
+DES_cblock key="1234567"; // 密钥
+DES_key_schedule key_schedule; // 解密用schedule
+unsigned  char input[MAX]={'\0'}; // 加密输入/解密输出
+size_t len_cry = (MAX + 7) / 8 * 8; // 定义输出长度
+DES_cblock ivec; // IV
+char judge[5]={'\0'}; // 程序判别位
+int judge_flag=0; // 判别结果储存
+int encryd=0; // 解密使能位
+int write_file_enable=0;
+char save_file[2]={'\0'};
+int n_write; // 写标志位
+char check_flag[128]={'\0'};
+char check[2]={'\0'};
+char data_store[MAX]={'\0'};
+int count_en=0;
+
+//功能函数
+// 模式判别
+int judge_prog(void)
+{
+	count = 0;
+	for(i=0;i<4;i++)
+        {
+        	if(end_flag[i]==judge[i])
+        	{
+        		count++;
+        	}
+        	else
+        	break;
+        }
+        if(count==4)
+        {
+        	return -1;
+        }
+	count = 0;
+	for(i=0;i<5;i++)
+        {
+        	if(exit_flag[i]==judge[i])
+        	{
+        		count++;
+        	}
+        	else
+        	break;
+        }
+        if(count==5)
+        {
+        	return 1;
+        }
+	return 0;
+}
+
+// 解密函数
+void decry(void)
+{
+	DES_set_key(&key, &key_schedule); // 根据key产生schedule
+	memset((char*)&ivec, 0, sizeof(ivec)); //设置IV为0x0000000000000000
+	unsigned char* output = new unsigned char[len_cry + 1]; // 加密输出/解密输入
+	memset(judge,0,sizeof(judge)); // 清空数组
+	for(i=0;i<5;i++)
+	{
+		judge[i]=readbuf[i];
+	} // 将读数组数据传递给解密输入
+	memcpy(output,readbuf,MAX);
+	memset(readbuf,0,sizeof(readbuf));
+	DES_ncbc_encrypt(output, input, len_cry, &key_schedule, &ivec, 0); // 解密
+	memcpy(readbuf,input,MAX);
+	judge_flag=judge_prog();
+	if((judge_flag==-1)||(judge_flag==1));
+	else
+	{
+		printf("解密结果：\n");
+		printf("%s\n",readbuf); // 打印解密结果
+	}
+	memset(data_store,0,sizeof(data_store));
+	strcpy(data_store,readbuf);
+}
+
+void decry_for_check(void)
+{
+	DES_set_key(&key, &key_schedule); // 根据key产生schedule
+	memset((char*)&ivec, 0, sizeof(ivec)); //设置IV为0x0000000000000000
+	unsigned char* output = new unsigned char[len_cry + 1]; // 加密输出/解密输入
+	memset(judge,0,sizeof(judge)); // 清空数组
+	memcpy(output,readbuf,MAX);
+	memset(readbuf,0,sizeof(readbuf));
+	DES_ncbc_encrypt(output, input, len_cry, &key_schedule, &ivec, 0); // 解密
+	memcpy(readbuf,input,MAX);
+	memset(data_store,0,sizeof(data_store));
+	strcpy(data_store,readbuf);
+}
+
+int login(void)
+{
+	//char admin[10]={'a','d','m','i','n'};
+	//char admin_key[10]={'2','0','0','2','0','9'};
+	char user[5][10]={{'1','0','0','8','6'},{'3','0','0','1','8','8'},{'0','5','9','9'},{'1','5','2','5','9','2'},{'1','8','3','5','0','9'}};
+	char key[5][10]={{'0','0','0','0','0','0'},{'1','1','1','1','1','1'}};
+	char login_user[10]={'\0'};
+	char login_key[10]={'\0'};
+	char user_temp[10]={'\0'};
+	char key_temp[10]={'\0'};
+	int m=0;
+	int n=0;
+	int user_flag=0;
+	int key_flag=0;
+	memset(writebuf,0,sizeof(writebuf));
+	strcpy(writebuf,"请输入账号：");
+	n_write=write(c_fd,writebuf,sizeof(writebuf)); // 向client端发送数据
+	memset(readbuf,0,sizeof(readbuf));
+	read(c_fd,readbuf,sizeof(readbuf)); // 读操作
+	strcpy(login_user,readbuf);
+	memset(writebuf,0,sizeof(writebuf));
+	strcpy(writebuf,"请输入密码：");
+	n_write=write(c_fd,writebuf,sizeof(writebuf)); // 向client端发送数据
+	memset(readbuf,0,sizeof(readbuf));
+	read(c_fd,readbuf,sizeof(readbuf)); // 读操作
+	strcpy(login_key,readbuf);
+	for(m=0;m<5;m++)
+	{
+		for(n=0;n<10;n++)
+		{
+			user_temp[n]=user[m][n];
+			key_temp[n]=key[m][n];
+		}
+		if(strcmp(user_temp,login_user)==0)
+		{
+			user_flag=1;
+			if(strcmp(key_temp,login_key)==0)
+			{
+				memset(writebuf,0,sizeof(writebuf));
+				strcpy(writebuf,"登录成功！"); 
+				n_write=write(c_fd,writebuf,sizeof(writebuf)); // 向client端发送数据
+				key_flag=1;
+				return 1;
+			}
+			else
+			{
+				memset(writebuf,0,sizeof(writebuf));
+				strcpy(writebuf,"错误的密码！");
+				n_write=write(c_fd,writebuf,sizeof(writebuf)); // 向client端发送数据
+				return 0;
+			}
+		}
+		else
+		{
+			user_flag=0;
+		}
+	 } 
+	if(user_flag==0)
+	{
+		memset(writebuf,0,sizeof(writebuf));
+		strcpy(writebuf,"错误的账号！");
+		n_write=write(c_fd,writebuf,sizeof(writebuf)); // 向client端发送数据
+	}
+	return 0;
+}
+
+void md5()
+{
+	MD5_CTX  ctx;
+	char  data[MAX];
+	unsigned char  md[MD5_LENGTH];
+	char buf[MAX] = "";
+	char tmp[3] = "";
+	int q=0;
+	strcpy(data,data_store);
+	data[strlen(data)-1] = '\0';
+	MD5_Init(&ctx);
+	MD5_Update(&ctx, data, strlen(data));
+	MD5_Final(md, &ctx);
+	for (q=0; q < MD5_LENGTH; q++)
+	{
+        sprintf(tmp, "%02X", md[q]);
+        strcat(buf, tmp);
+    	}
+	printf("完整性验证序列: ");
+	memset(check_flag,0,sizeof(check_flag));
+	strcpy(check_flag,buf);
+	printf("%s\n",check_flag);
+}
+	
+int main(int argc,char** argv)
+{
+ 
+        if(argc !=3)
+	{
+                puts("调用格式不正确！\n"); // 调用格式出错时提示并退出
+                exit(-1);
+        }
+        s_fd=socket(AF_INET,SOCK_STREAM,0); // 服务端套接字产生
+        s_addr.sin_family=AF_INET; // IPV4
+        s_addr.sin_port=htons(atoi(argv[2])); // 端口号
+        s_addr.sin_addr.s_addr=inet_addr(argv[1]); // 服务端地址 
+        bind(s_fd,(struct sockaddr *)&s_addr,sizeof(struct sockaddr_in)); // 地址绑定
+        listen(s_fd,10); // 启用监听
+        socklen_t len=sizeof(struct sockaddr_in); // 输入地址长度
+        c_fd=accept(s_fd,(struct sockaddr *)&c_addr,&len); // 等待客户端连接，返回客户端套接字
+        //puts("connected successfully"); // 成功连接提示
+        printf("clientIP:%s\n",inet_ntoa(c_addr.sin_addr)); // 打印客户端IP地址
+	int login_flag=0;
+	int login_count=0;
+	puts("客户端登录中……");
+	while(1)
+	{
+		login_flag=login();
+		if(login_flag==1)
+		break;
+		else
+		login_count++;
+		if(login_count==3)
+		{
+			puts("登录失败，已锁定");
+			memset(writebuf,0,sizeof(writebuf));
+			strcpy(writebuf,"登录失败，已锁定");
+			n_write=write(c_fd,writebuf,sizeof(writebuf)); // 向client端发送数据
+			exit(-1);
+		}
+	}
+	memset(readbuf,0,sizeof(readbuf));
+	read(c_fd,readbuf,sizeof(readbuf)); // 读操作
+	printf("%s",readbuf); // 打印收到的信息
+	memset(readbuf,0,sizeof(readbuf));
+	read(c_fd,readbuf,sizeof(readbuf)); // 读操作
+	printf("%s",readbuf); // 打印收到的信息
+	if(strcmp(readbuf,"CPUID:810F8100-FFFB8B07\n")==0)
+	{
+		memset(readbuf,0,sizeof(readbuf));
+		read(c_fd,readbuf,sizeof(readbuf)); // 读操作
+		printf("%s",readbuf); // 打印收到的信息
+		if(strcmp(readbuf,"eth: lo, mac: 00:00:00:00:00:00\n")==0)
+		{
+			memset(readbuf,0,sizeof(readbuf));
+			read(c_fd,readbuf,sizeof(readbuf)); // 读操作
+			printf("%s",readbuf); // 打印收到的信息
+			if(strcmp(readbuf,"eth: virbr0, mac: 52:54:00:d6:a2:30\n")==0)
+			{
+				puts("硬件校验通过（CPU ID与MAC均合法）！");
+				memset(writebuf,0,sizeof(writebuf));
+				strcpy(writebuf,"您的硬件设备具有接入权限！");
+				n_write=write(c_fd,writebuf,sizeof(writebuf)); // 向client端发送数据
+			}
+			else
+			{
+				memset(writebuf,0,sizeof(writebuf));
+				strcpy(writebuf,"您的硬件设备无接入权限！");
+				n_write=write(c_fd,writebuf,sizeof(writebuf)); // 向client端发送数据
+				exit(-1);
+			}
+		}
+		else
+		{
+			memset(writebuf,0,sizeof(writebuf));
+			strcpy(writebuf,"您的硬件设备无接入权限！");
+			n_write=write(c_fd,writebuf,sizeof(writebuf)); // 向client端发送数据
+			exit(-1);
+		}
+	}
+	else
+	{
+		memset(writebuf,0,sizeof(writebuf));
+		strcpy(writebuf,"您的硬件设备无接入权限！");
+		n_write=write(c_fd,writebuf,sizeof(writebuf)); // 向client端发送数据
+		exit(-1);
+	}
+	while(1)
+	{
+		memset(readbuf,0,sizeof(readbuf));
+		read(c_fd,readbuf,sizeof(readbuf)); // 读操作
+		if((strcmp(readbuf,"D")==0)||(strcmp(readbuf,"E")==0)||(strcmp(readbuf,"F")==0))
+		break;
+		else
+		printf("%s",readbuf); // 打印收到的信息
+	}
+        while(1)
+	{
+		if((strcmp(readbuf,"D")==0)||(strcmp(readbuf,"E")==0)||(strcmp(readbuf,"F")==0));
+		else
+                read(c_fd,readbuf,sizeof(readbuf)); // 读操作
+		if(readbuf[0]=='D'&&readbuf[1]=='\0')
+		{
+			write_file_enable=0;
+			puts("当前工作在数据传输模式"); // 客户端单独传递D，指示数据传输模式
+		}
+		else if(readbuf[0]=='F'&&readbuf[1]=='\0')
+		{
+			write_file_enable=1;
+			puts("当前工作在文件传输模式"); // 客户端单独传递F，指示数据传输模式
+		}
+		else if(readbuf[0]=='Y'&&readbuf[1]=='\0')
+		{
+			encryd=1; // 加密标志位置1，传递的是加密内容
+			puts("加密传输"); // 提示用户当前加密传输
+			count_en=0;
+		} // 客户端单独传递Y，指示加密模式
+		else if(readbuf[0]=='N'&&readbuf[1]=='\0')
+		encryd=0; // 客户端单独传递N，指示无加密
+		else if(readbuf[0]=='E'&&readbuf[1]=='\0')
+		{
+			exit(-1);
+		}
+		else if(encryd==1)
+		{
+			for(i=0;i<5;i++)
+			{
+				judge[i]=readbuf[i];
+			} 
+			judge_flag=judge_prog();
+			if(judge_flag==-1)
+			exit(-1);
+			else if(judge_flag==1);
+			else
+			{
+				puts("是否解密（Y：是；N：否）：");
+				scanf("%s",decry_flag);
+				if(decry_flag[0]=='Y')
+				{
+					decry();
+					if(judge_flag==-1)
+					exit(-1);
+				} // 用户指定解密
+				else
+				{
+					for(i=0;i<5;i++)
+					judge[i]=readbuf[i];
+					judge_flag=judge_prog();
+					if(judge_flag==-1)
+					exit(-1);
+					else if(judge_flag==1);
+					else
+		        	        printf("%s\n",readbuf); // 打印收到的信息
+				}
+				puts("是否保存到文件：");
+				scanf("%s",save_file);
+				if(save_file[0]=='Y')
+				{
+					puts("保存到文件：");
+					scanf("%s", filename_write);
+					if((fp = fopen(filename_write, "w")) == NULL)
+					{
+						printf("文件打开失败。\n");
+					}
+					else
+					{
+						fputs(readbuf,fp); 
+						fclose(fp);
+						puts("成功写入！");
+					}
+				}
+				puts("是否校验完整性：");
+				scanf("%s",check);
+				if(check[0]=='Y')
+				{
+					if(decry_flag[0]=='N')
+					decry_for_check();
+					memset(writebuf,0,sizeof(writebuf));
+					strcpy(writebuf,"check");
+					n_write=write(c_fd,writebuf,sizeof(writebuf)); // 向client端发送数据
+					md5();
+					memset(readbuf,0,sizeof(readbuf)); // 清空数组
+					//read(c_fd,readbuf,sizeof(readbuf)); // 读操作
+					/*if(write_file_enable&&encryd&&(count_en==0))
+					{
+						count_en++;
+						read(c_fd,readbuf,sizeof(readbuf)); // 读操作
+					}*/
+					read(c_fd,readbuf,sizeof(readbuf)); // 读操作
+					if(strcmp(readbuf,check_flag)==0)
+					puts("传输完整");
+					else
+					puts("传输不完整");
+				}
+			}
+		} // 加密模式下询问用户是否解密，并打印输出
+		else
+		{
+			for(i=0;i<5;i++)
+			{
+				judge[i]=readbuf[i];
+			} 
+			judge_flag=judge_prog();
+			if(judge_flag==-1)
+			exit(-1);
+			else if(judge_flag==1);
+			else
+			{
+				printf("%s\n",readbuf); // 无加密传输时，直接打印收到的信息
+				strcpy(data_store,readbuf);
+				puts("是否保存到文件：");
+				scanf("%s",save_file);
+				if(save_file[0]=='Y')
+				{
+					puts("保存到文件：");
+					scanf("%s", filename_write);
+					if((fp = fopen(filename_write, "w")) == NULL)
+					{
+						printf("文件打开失败。\n");
+					}
+					else
+					{
+						fputs(readbuf,fp); 
+						fclose(fp);
+						puts("成功写入！");
+					}
+				}
+				puts("是否校验完整性：");
+				scanf("%s",check);
+				if(check[0]=='Y')
+				{
+					memset(writebuf,0,sizeof(writebuf));
+					strcpy(writebuf,"check");
+					n_write=write(c_fd,writebuf,sizeof(writebuf)); // 向client端发送数据
+					md5();
+					memset(readbuf,0,sizeof(readbuf)); // 清空数组
+					read(c_fd,readbuf,sizeof(readbuf)); // 读操作
+					if(strcmp(readbuf,check_flag)==0)
+					puts("传输完整");
+					else
+					puts("传输不完整");
+				}
+			}
+		}
+                memset(readbuf,0,sizeof(readbuf)); // 清空数组
+        }
+ 
+        return 0;
+}
